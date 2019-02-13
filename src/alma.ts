6541c47646822b7a1b32720d05a257db1b851ba4
Eli Zoller <zoller.eli@gmail.com>
2019-02-13 10:01:23 -0700

    update alma module

--- src/alma.ts
+++ src/alma.ts
@@ -1,5 +1,7 @@
 "use strict";
+import { createHmac } from "crypto";
 import { NextFunction, Request, Response } from "express";
+import { config } from "./../config";
 
 export let getWebhook = (req: Request, res: Response, next: NextFunction) => {
     // tslint:disable-next-line:no-console
@@ -10,4 +12,19 @@ export let getWebhook = (req: Request, res: Response, next: NextFunction) => {
 export let postWebhook = (req: Request, res: Response, next: NextFunction) => {
     // tslint:disable-next-line:no-console
     console.log("POST request received");
+    const secret = config.dev.alma.webhook_secret;
+    if (!req.get("X-Exl-Signature") || !validateSignature(req.body,
+        secret,
+        req.get("X-Exl-Signature"))) {
+        return res.status(401).send({ errorMessage: "Invalid Signature" });
+    }
+
+    res.json({ message: "Successful webhook received", action: req.body.action });
 };
+
+function validateSignature(body: any, secret: string, signature: string) {
+    const hash = createHmac("SHA256", secret.toString())
+        .update(JSON.stringify(body))
+        .digest("base64");
+    return (hash === signature);
+}
