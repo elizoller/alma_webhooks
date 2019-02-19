02656731a16f0f42389b5c72fb9d195ce8279b75
Eli Zoller <zoller.eli@gmail.com>
2019-02-19 12:02:32 -0800

    fix config location compilation

--- src/alma.ts
+++ src/alma.ts
@@ -1,7 +1,7 @@
 "use strict";
 import { createHmac } from "crypto";
 import { NextFunction, Request, Response } from "express";
-import { config } from "./../config";
+import { config } from "./config";
 
 export let getWebhook = (req: Request, res: Response, next: NextFunction) => {
     // tslint:disable-next-line:no-console
@@ -13,9 +13,8 @@ export let postWebhook = (req: Request, res: Response, next: NextFunction) => {
     // tslint:disable-next-line:no-console
     console.log("POST request received");
     const secret = config.dev.alma.webhook_secret;
-    if (!req.get("X-Exl-Signature") || !validateSignature(req.body,
-        secret,
-        req.get("X-Exl-Signature"))) {
+    const sign = req.get("X-Exl-Signature").toString();
+    if (!sign || !validateSignature(req.body, secret, sign)) {
         return res.status(401).send({ errorMessage: "Invalid Signature" });
     }
 
